8768b47d355937db6734fa781e699e5926c99b0d
phatienza-aws <paulhenry.atienza@awsys-i.com>
2018-09-11 18:17:16 +0800

    changed login json response

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -51,12 +51,12 @@ exports.userLogin = (req, res, next) => {
       const token = jwt.sign(
         { email: fetchedUser.email, userId: fetchedUser._id },
         process.env.AUTH_TOKEN,
-        { expiresIn: "1h" }
+        { expiresIn: "1h" },
       );
       res.status(200).json({
         token: token,
         expiresIn: 3600,
-        userId: fetchedUser._id
+        user: fetchedUser
       });
     })
     .catch(err => {
